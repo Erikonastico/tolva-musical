84964e3afa7730d574ecf143c6ac490b6a4f0ed3
Erikonastico <39448705+Erikonastico@users.noreply.github.com>
2021-12-15 07:58:37 -0300

    Add files via upload

--- auxbackend/database.js
+++ auxbackend/database.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
 const roteador = require('./rotas/pessoas')
 app.use('/backend/pessoas', roteador);
 
-app.listen(3000, () => console.log('A API está funcionando.'));
+app.listen(PORT, () => console.log('A API está funcionando.'));
