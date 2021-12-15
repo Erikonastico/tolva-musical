45d476f8640836975cb6ea78560943f42d52fadc
Erikonastico <39448705+Erikonastico@users.noreply.github.com>
2021-12-15 08:02:15 -0300

    Update database.js

--- auxbackend/database.js
+++ auxbackend/database.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 const roteador = require('./rotas/pessoas')
-app.use('/backend/pessoas', roteador);
+app.use('/auxbackend/pessoas', roteador);
 
 app.listen(PORT, () => console.log('A API está funcionando.'));
